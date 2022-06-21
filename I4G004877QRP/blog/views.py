5bc23027163d2b825e551ca38cbb40e8dd33cda3
saheedibrahim <saolabram@gmail.com>
2022-06-21 17:27:54 +0100

    update views.py

--- I4G004877QRP/blog/views.py
+++ I4G004877QRP/blog/views.py
@@ -1,3 +1,22 @@
+from audioop import reverse
+from dataclasses import fields
+import genericpath
+from pyexpat import model
+# from typing import Generic
 from django.shortcuts import render
 
+from I4G004877QRP.blog.models import Post
+
 # Create your views here.
+class PostListView(Generic.ListView):
+    model = Post
+
+class PostCreateView(Generic.CreateView):
+    model = Post
+    fields = "--all--"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(Generic.DetailView):
+    model = Post
+
+class PostUpdateView(Generic.UpdateView)
